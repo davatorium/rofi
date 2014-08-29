f41657476e02056d26705484860191ba3c04150a
QC <qball@gmpclient.org>
2014-08-29 15:21:10 +0200

    Add missing test file.

---
+++ test/run_errormsg_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Create fake X
+Xvfb :200 &
+XPID=$!
+
+# wait till it is up, run rofi with error message
+sleep 1 && ./rofi -e "Printing error message" -display :200 & 
+RPID=$!
+
+# send enter.
+sleep 5 && DISPLAY=:200 xdotool key Return
+
+#  Get result, kill xvfb
+wait ${RPID}
+RETV=$?
+kill ${XPID}
+exit ${RETV}
