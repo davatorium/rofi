a38ddb3b06a6bbd51c61767e0066faf60f65fbc3
QC <qball@gmpclient.org>
2015-04-14 22:12:21 +0200

    Fix space.

--- source/helper.c
+++ source/helper.c
@@ -3,7 +3,7 @@
  *
  * MIT/X11 License
  * Copyright (c) 2012 Sean Pringle <sean.pringle@gmail.com>
- * Modified 2013-2015 Qball  Cow <qball@gmpclient.org>
+ * Modified 2013-2015 Qball Cow <qball@gmpclient.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
