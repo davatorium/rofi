27f5c7413e75d2f381ea4d2d7affcc27704d769f
QC <qball@gmpclient.org>
2015-09-02 12:29:28 +0200

    cleanup.

--- include/history.h
+++ include/history.h
@@ -1,29 +1,3 @@
-/**
- * rofi
- *
- * MIT/X11 License
- * Copyright 2013-2015 Qball Cow <qball@gmpclient.org>
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- */
 #ifndef ROFI_HISTORY_H
 #define ROFI_HISTORY_H
 
@@ -34,8 +8,7 @@
  * Sets the entry in the history, if it exists its use-count is incremented.
  *
  */
-void history_set ( const char *filename, const char *entry )
-__attribute__( ( nonnull ) );
+void history_set ( const char *filename, const char *entry ) __attribute__( ( nonnull ) );
 
 
 /**
@@ -44,8 +17,7 @@ __attribute__( ( nonnull ) );
  *
  * Removes the entry from the history.
  */
-void history_remove ( const char *filename, const char *entry )
-__attribute__( ( nonnull ) );
+void history_remove ( const char *filename, const char *entry ) __attribute__( ( nonnull ) );
 
 
 /**
@@ -55,9 +27,6 @@ __attribute__( ( nonnull ) );
  * Gets the entries in the list (in order of usage)
  * @returns a list of entries length long. (and NULL terminated).
  */
-char ** history_get_list ( const char *filename, unsigned int * length )
-__attribute__( ( nonnull ) );
-
-
+char ** history_get_list ( const char *filename, unsigned int * length ) __attribute__( ( nonnull ) );
 
 #endif
