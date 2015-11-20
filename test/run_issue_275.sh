264e9e5b0e4dafd158b0ee9a922f7fc641614b6c
Qball Cow <qball@gmpclient.org>
2015-11-20 11:15:34 +0100

    Issue #275 test case

---
+++ test/run_issue_275.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo -e "aap\nnoot\nmies" | rofi -dmenu &
+RPID=$!
+
+# send enter.
+sleep 5;
+xdotool key 't' 
+sleep 0.4
+xdotool key 'r'
+sleep 0.4
+xdotool key 'u'
+sleep 0.4
+xdotool key 'e'
+sleep 0.4
+xdotool key End 
+
+#  Get result, kill xvfb
+wait ${RPID}
+RETV=$?
+
+exit ${RETV}
