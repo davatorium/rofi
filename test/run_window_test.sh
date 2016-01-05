155cf351e08b88c88f6d188484e83417ca33fa2a
Dave Davenport <qball@gmpclient.org>
2016-01-05 18:19:35 +0100

    Update window test.

--- test/run_window_test.sh
+++ test/run_window_test.sh
@@ -2,20 +2,24 @@
 
 # wait till it is up, run rofi with error message
 sleep 1;
-xterm &
+xterm -T MonkeySee sh &
 XPID=$!
 sleep 1;
-echo -e -n "aap\nnoot\nmies" | rofi -modi window -show window > output.txt & 
+xterm -T TermUnwanted sh &
+TPID=$!
+sleep 1;
+rofi -modi window -show window > output.txt &
 RPID=$!
 
 # send enter.
 sleep 5;
-xdotool type 'xterm' 
+xdotool type 'MonkeySee'
 sleep 0.4
 xdotool key Return
 sleep 1;
 xdotool key Ctrl+d
 wait ${XPID}
+kill ${TPID}
 #  Get result, kill xvfb
 wait ${RPID}
 RETV=$?
