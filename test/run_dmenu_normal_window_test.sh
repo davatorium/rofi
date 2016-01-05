933eb7e490dd49c4e0031758cf14a1680aec979c
Dave Davenport <qball@gmpclient.org>
2016-01-05 17:14:47 +0100

    Fix normal window test

--- test/run_dmenu_normal_window_test.sh
+++ test/run_dmenu_normal_window_test.sh
@@ -3,15 +3,9 @@
 # wait till it is up, run rofi with error message
 sleep 1;
 echo -e -n "aap\nnoot\nmies" | rofi -dmenu  -normal-window  > output.txt & 
-sleep 1
-echo "move"
-xdotool mousemove 10 10
-sleep 1
-echo "click"
-xdotool click 1
 RPID=$!
 sleep 4
-#xdotool getactivewindow windowsize 100% 100%
+xdotool getactivewindow windowsize 100% 100%
 echo "Window resized"
 # send enter.
 sleep 1
