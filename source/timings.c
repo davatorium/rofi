e39f2dd53cb1f1fcebbf090f84c9e96d1de9daa5
Dave Davenport <qball@gmpclient.org>
2016-01-14 17:44:58 +0100

    Fix compiling with timings enabled

--- source/timings.c
+++ source/timings.c
@@ -23,6 +23,7 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#include <stdio.h>
 #include "rofi.h"
 #include "timings.h"
 #if TIMINGS
