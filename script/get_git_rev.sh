93362d63f2446fe9d1968eccf0f119516da3bf3a
Dave Davenport <qball@gmpclient.org>
2016-04-10 13:22:49 +0200

    Update git revision script.

--- script/get_git_rev.sh
+++ script/get_git_rev.sh
@@ -2,18 +2,19 @@
 
 DIR=$1
 FILE=$2
+GIT=$(which git)
 
-if [ -d "${DIR}/.git/" ]
+if [ -d "${DIR}/.git/" ] && [ -n "${GIT}" ]
 then
     echo -n "#define GIT_VERSION \""  > ${FILE}.tmp
-    REV="$(git describe --tags --always --dirty) ($(git describe --tags --always --all | sed -e 's:heads/::'))"
+    REV="$(${GIT} describe --tags --always --dirty) ($(${GIT} describe --tags --always --all | sed -e 's:heads/::'))"
     echo -n "${REV}" >> ${FILE}.tmp
     echo "\"" >> ${FILE}.tmp
 else
     echo "#undef GIT_VERSION" > ${FILE}.tmp
 fi
 
-if ! diff ${FILE}.tmp ${FILE} > /dev/null 
+if [ ! -f ${FILE} ] || ! diff ${FILE}.tmp ${FILE} > /dev/null
 then
     mv ${FILE}.tmp ${FILE}
 else
