a90ff4803252d2ec43f322281771f4b90fa25b44
Dave Davenport <qball@gmpclient.org>
2016-08-29 08:46:21 +0200

    But broken drun under a configure option.

--- source/rofi.c
+++ source/rofi.c
@@ -368,12 +368,12 @@ static int add_mode ( const char * token )
         modi[num_modi] = &run_mode;
         num_modi++;
     }
-    /*
+#ifdef BROKEN_DRUN
     else if ( strcasecmp ( token, "drun" ) == 0 ) {
         modi[num_modi] = &drun_mode;
         num_modi++;
     }
-    */
+#endif
     // combi dialog
     else if ( strcasecmp ( token, "combi" ) == 0 ) {
         modi[num_modi] = &combi_mode;
