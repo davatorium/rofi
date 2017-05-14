3fe42c3f79792b3fa07c865766e8128dc1c3d6aa
Dave Davenport <qball@gmpclient.org>
2017-05-14 19:43:44 +0200

    Update old-theme-convert test

--- test/convert_old_theme_test.sh
+++ test/convert_old_theme_test.sh
@@ -2,11 +2,12 @@
 
 TOP_DIR=$1
 
+xrdb -retain -load ${TOP_DIR}/doc/old-theme-convert-input.theme
 rofi -config ${TOP_DIR}/doc/old-theme-convert-input.theme -dump-theme  > temp.txt
 
 if ! diff temp.txt ${TOP_DIR}/doc/old-theme-convert-output.rasi > /dev/null
 then
-    echo "Dump default theme does not match."
+    echo "Convert default theme failed"
     diff temp.txt ${TOP_DIR}/doc/old-theme-convert-output.rasi
     exit 1;
 fi
