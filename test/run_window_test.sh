e570fd76c32678568afec9fd02a9d2cf0fccd251
Quentin Glidic <sardemff7+git@sardemff7.net>
2017-07-27 18:08:23 +0200

    tests/window: Add some logging
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- test/run_window_test.sh
+++ test/run_window_test.sh
@@ -4,12 +4,15 @@
 sleep 1;
 xterm -T MonkeySee sh &
 XPID=$!
+echo "Started MonkeySee xterm: pid ${XPID}"
 sleep 1;
 xterm -T TermUnwanted sh &
 TPID=$!
+echo "Started TermUnwanted xterm: pid ${TPID}"
 sleep 1;
 rofi -modi window -show window > output.txt &
 RPID=$!
+echo "Started rofi: pid ${RPID}"
 
 # send enter.
 sleep 5;
@@ -19,9 +22,16 @@ xdotool key Return
 sleep 1;
 xdotool key Ctrl+d
 sleep 1;
-kill ${TPID}
+
+echo -n "Killing TermUnwanted: "
+if kill ${TPID}; then
+    echo "done"
+#    wait ${TPID}
+fi
+
 if pgrep -u $USER xterm
 then
+    echo "Found remaining xterms: $(pgrep -u $USER xterm)"
     kill ${XPID}
     kill ${RPID}
     exit 1
