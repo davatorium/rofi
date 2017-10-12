a723356d146a0e757a98ad25f8cbd17d176dbb46
Quentin Glidic <sardemff7+git@sardemff7.net>
2017-10-12 19:53:15 +0200

    bindings: Fix API usage
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- source/rofi.c
+++ source/rofi.c
@@ -810,7 +810,7 @@ int main ( int argc, char *argv[] )
 
     TICK_N ( "Setup mainloop" );
 
-    bindings = nk_bindings_new ();
+    bindings = nk_bindings_new ( -1 );
     TICK_N ( "NK Bindings" );
 
     if ( !display_setup ( main_loop, bindings ) ) {
