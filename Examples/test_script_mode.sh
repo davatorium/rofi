974b4825008f477f231e7c702febc20a361f1fc2
Dave Davenport <qball@gmpclient.org>
2017-11-18 14:44:40 +0100

    [ScriptMode] Add markup to message test in script mode test.

--- Examples/test_script_mode.sh
+++ Examples/test_script_mode.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo -en "\0prompt\x1ftesting\n"
+echo -en "\x00prompt\x1ftesting\n"
 echo -en "\0urgent\x1f0,2\n"
 echo -en "\0active\x1f1\n"
-echo -en "\0message\x1fSpecial message\n"
+echo -en "\0message\x1fSpecial <b>bold</b>message\n"
 
 echo "aap"
 echo "noot"
