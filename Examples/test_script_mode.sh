75ba12c51673012131544d494a69f817f656fe5b
Dave Davenport <qball@gmpclient.org>
2018-01-05 23:15:26 +0100

    Extend script example with markup-rows

--- Examples/test_script_mode.sh
+++ Examples/test_script_mode.sh
@@ -3,12 +3,14 @@
 echo -en "\x00prompt\x1ftesting\n"
 echo -en "\0urgent\x1f0,2\n"
 echo -en "\0active\x1f1\n"
+echo -en "\0markup-rows\x1ftrue\n"
 echo -en "\0message\x1fSpecial <b>bold</b>message\n"
 
 echo "aap"
 echo "noot"
 echo "mies"
 echo "testing"
+echo "<b>Bold</b>"
 if [ -n "$@" ]
 then
     echo "$@"
