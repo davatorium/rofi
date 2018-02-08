0f1717f829e95d7f3e1ac5b213d3f73f09b0da62
Dave Davenport <qball@gmpclient.org>
2018-02-08 17:56:31 +0100

    [Script] Reset highlighting when reload view
    Issue: #767

--- Examples/test_script_mode.sh
+++ Examples/test_script_mode.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
-echo -en "\x00prompt\x1ftesting\n"
-echo -en "\0urgent\x1f0,2\n"
-echo -en "\0active\x1f1\n"
-echo -en "\0markup-rows\x1ftrue\n"
-echo -en "\0message\x1fSpecial <b>bold</b>message\n"
-
-echo "aap"
-echo "noot"
-echo "mies"
-echo "testing"
-echo "<b>Bold</b>"
-if [ -n "$@" ]
+if [ x"$@" = x"quit" ]
 then
-    echo "$@"
+    exit 0
+fi
+
+if [ "$@" ]
+then
+    for a in {1..10}
+    do
+        echo "$a"
+    done
+    echo "quit"
+else
+    echo -en "\x00prompt\x1ftesting\n"
+    echo -en "\0urgent\x1f0,2\n"
+    echo -en "\0active\x1f1\n"
+    echo -en "\0markup-rows\x1ftrue\n"
+    echo -en "\0message\x1fSpecial <b>bold</b>message\n"
+
+    echo "aap"
+    echo "noot"
+    echo "mies"
+    echo "testing"
+    echo "<b>Bold</b>"
+    echo "quit"
 fi
