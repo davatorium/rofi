1ed94ead7e20161482bccbaa0eeb1f3006c743d4
Dave Davenport <qball@gmpclient.org>
2018-11-01 09:32:00 +0100

    Add missing Example script
    Fixes: #869

---
+++ Examples/i3_empty_workspace.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MAX_DESKTOPS=20
+
+WORKSPACES=$(seq -s '\n' 1 1 ${MAX_DESKTOPS})
+
+EMPTY_WORKSPACE=$( (i3-msg -t get_workspaces | tr ',' '\n' | grep num | awk -F:  '{print int($2)}' ; \
+            echo -e ${WORKSPACES} ) | sort -n | uniq -u | head -n 1)
+
+i3-msg workspace ${EMPTY_WORKSPACE}
