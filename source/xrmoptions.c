f19492cc4cdc21c134f9d80ff0438e71970414e2
Jason Kim <git@jasonk.me>
2020-05-15 00:41:19 -0700

    refactor: don't handle mem in xrm_Char case
    `mem` shouldn't ever be set when `XrmOption` is `type` `xrm_Char`.
    Therefore, there is no need to check it and free it.  Remove that logic.

--- source/xrmoptions.c
+++ source/xrmoptions.c
@@ -482,13 +482,7 @@ static gboolean __config_parser_set_property ( XrmOption *option, const Property
             *error = g_strdup_printf ( "Option: %s needs to be set with a string not a %s.", option->name, PropertyTypeName[p->type] );
             return TRUE;
         }
-
         gchar value = *( p->value.s );
-        if ( ( option )->mem != NULL ) {
-            g_free ( option->mem );
-            option->mem = NULL;
-        }
-
         *( option->value.charc ) = value;
         option->source  = CONFIG_FILE_THEME;
     }
