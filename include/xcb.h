f14c5c2ada07d3c1c392b25846a59590cc12c8e6
Dave Davenport <qball@gmpclient.org>
2021-05-31 14:02:08 +0200

    [X11Helper] Add exception for bspwm window placing above window.
    fixes: #1169

--- include/xcb.h
+++ include/xcb.h
@@ -208,6 +208,8 @@ typedef enum
     WM_DO_NOT_CHANGE_CURRENT_DESKTOP = 1,
     /** PANGO WORKSPACE NAMES */
     WM_PANGO_WORKSPACE_NAMES         = 2,
+    /** Root window offset (for bspwm) */
+    WM_ROOT_WINDOW_OFFSET            = 4,
 } WindowManagerQuirk;
 
 /**
