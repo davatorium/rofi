497d18d108bb6e810c61ef07d864514fca323474
Dave Davenport <qball@blame.services>
2022-03-02 22:06:04 +0100

    [Script] Support a data option that get passed to next execution.
    fixes: #1601

--- Examples/test_script_env.sh
+++ Examples/test_script_env.sh
@@ -7,10 +7,15 @@ then
 fi
 
 echo -en "\x00no-custom\x1ffalse\n"
+echo -en "\x00data\x1fmonkey do, monkey did\n"
 echo -en "\x00use-hot-keys\x1ftrue\n"
 echo -en "${ROFI_RETV}\x00icon\x1ffirefox\x1finfo\x1ftest\n"
 
 if [ -n "${ROFI_INFO}" ]
 then
     echo "my info: ${ROFI_INFO} "
+fi
+if [ -n "${ROFI_DATA}" ]
+then
+    echo "my data: ${ROFI_DATA} "
 fi
