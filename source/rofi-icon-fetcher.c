f539a082228aef10861ea8742f92938c4c8dd162
lbonn <lbonn@users.noreply.github.com>
2024-02-16 18:41:07 +0100

    [Build] Reduce amount of warnings (#1944)
    * one unused parameter in recursivebrowser.c
    * overlength strings are overly pedantic, modern compilers support large
      strings
    * __FUNCTION__ creeped back in after #288

--- source/rofi-icon-fetcher.c
+++ source/rofi-icon-fetcher.c
@@ -361,7 +361,7 @@ static void rofi_icon_fetcher_worker(thread_state *sdata,
       icon_path, sentry->wsize, sentry->hsize, TRUE, &error);
   if (error != NULL) {
     g_warning("Failed to load image: |%s| %d %d %s (%p)", icon_path,
-              sentry->wsize, sentry->hsize, error->message, pb);
+              sentry->wsize, sentry->hsize, error->message, (void*)pb);
     g_error_free(error);
     if (pb) {
       g_object_unref(pb);
